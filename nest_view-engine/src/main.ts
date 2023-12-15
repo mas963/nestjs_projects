58569ff01bdb476d682b8332f7b4e48272947ec1
Yasar Can <38841941+mas963@users.noreply.github.com>
2023-12-15 18:09:04 +0300

    new layout, 404 page, page header... in short: view editing

--- nest_view-engine/src/main.ts
+++ nest_view-engine/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as hbs from 'hbs';
+import { NotFoundExceptionFilter } from './filters/notFoundException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
   hbs.registerPartials(join(__dirname, '..', 'views/partials'));
   hbs.registerPartials(join(__dirname, '..', 'views/layout'));
   app.setViewEngine('hbs');
+
+  app.useGlobalFilters(new NotFoundExceptionFilter());
 
   await app.listen(3000);
 }
